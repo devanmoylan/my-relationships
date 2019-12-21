c1bd5b88e7b1d1788c2d0b394421b60d13e6f4cb
Devan Moylan <dmmoylan@gmail.com>
2019-12-21 11:30:03 -0600

    Add rspec.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
 end
