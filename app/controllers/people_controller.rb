5707a2e0b93bf6adc7b47e9bd089f7b50af1af15
Devan Moylan <dmmoylan@gmail.com>
2020-01-28 11:46:12 -0600

    Add avatar to person.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -108,6 +108,6 @@ class PeopleController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def person_params
-    params.require(:person).permit(:first_name, :last_name, :birthday)
+    params.require(:person).permit(:first_name, :last_name, :birthday, :avatar)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def avatar(object, size = 40)
+    if object.avatar.attached?
+      object.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      gravatar_image_url(object.id.to_s, size: size)
+    end
+  end
 end
