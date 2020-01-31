7e8b39e401649b2c495ee8bf15771927107023da
Devan Moylan <dmmoylan@gmail.com>
2020-01-31 13:51:12 -0600

    Refactor mentions.

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,4 +1,7 @@
 class Mention < ApplicationRecord
+
+  has_rich_text :body
+
   belongs_to :mentionable, polymorphic: true
   belongs_to :sourceable, polymorphic: true
 end
---
+++ db/migrate/20200131193409_add_fields_to_mentions.rb
@@ -0,0 +1,7 @@
+class AddFieldsToMentions < ActiveRecord::Migration[6.0]
+  def change
+    add_column :mentions, :source_person_name, :string
+    add_column :mentions, :source_title, :string
+    add_column :mentions, :source_path, :string
+  end
+end
