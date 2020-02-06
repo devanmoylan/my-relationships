24e6c7517416b8ea9475f9b11429a48a70df4142
Devan Moylan <dmmoylan@gmail.com>
2020-02-06 16:13:17 -0600

    Redirect to people page after sign up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def the_person
     @person ||= the_user.people.find(params[:person_id] || params[:id])
   end
+
+  def after_sign_in_path_for(resource)
+    user_people_path(current_user)
+  end
 end
