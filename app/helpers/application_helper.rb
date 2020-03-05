31a2ed59305f9563c4eb039c9bfc857676e3a44c
Devan Moylan <dmmoylan@gmail.com>
2020-03-04 18:01:40 -0600

    Add datetime helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def bootstrap_class_for(flash_type)
-    {
-      success: 'alert-success',
-      error: 'alert-danger',
-      alert: 'alert-warning',
-      notice: 'alert-info'
-    }.stringify_keys[flash_type.to_s] || flash_type.to_s
-  end
+  include ActionView::Helpers::DateHelper
 
-  def row_color(date)
-    if date.today?
-      'table-danger'
-    elsif (Date.today..7.days.from_now).include?(date)
-      'table-warning'
-    end
+  def age_helper(birthday)
+    return nil if birthday.nil?
+
+    ((Date.today - birthday) / 365.25).to_i
   end
 
   def avatar(object, size = 40)
@@ -26,9 +17,29 @@ module ApplicationHelper
     end
   end
 
-  def age_helper(birthday)
-    return nil if birthday.nil?
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 
-    ((Date.today - birthday) / 365.25).to_i
+  def distance_of_time_in_words_to_now_helper(datetime)
+    if datetime.future?
+      distance_of_time_in_words_to_now(datetime) + " from now"
+    else
+      distance_of_time_in_words_to_now(datetime) + " ago"
+    end
+
+  end
+
+  def row_color(date)
+    if date.today?
+      'table-danger'
+    elsif (Date.today..7.days.from_now).include?(date)
+      'table-warning'
+    end
   end
 end
